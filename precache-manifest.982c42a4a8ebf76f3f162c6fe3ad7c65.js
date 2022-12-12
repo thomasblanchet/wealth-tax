c2ead848965b536ce05fc37becaaf266331f38e4
Thomas Blanchet <thomas.blanchet@wid.world>
2022-12-12 14:19:39 +0100

    Add redirect

---
+++ precache-manifest.982c42a4a8ebf76f3f162c6fe3ad7c65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8a1c00216e2851f90e9",
+    "url": "/wealth-tax/static/css/main.8b63febc.chunk.css"
+  },
+  {
+    "revision": "d8a1c00216e2851f90e9",
+    "url": "/wealth-tax/static/js/main.775fed52.chunk.js"
+  },
+  {
+    "revision": "037aa9ae5a55b22b6aaf",
+    "url": "/wealth-tax/static/js/runtime~main.97a278a9.js"
+  },
+  {
+    "revision": "d12abe0415b2f9113637",
+    "url": "/wealth-tax/static/js/2.65d2b4c0.chunk.js"
+  },
+  {
+    "revision": "84302996dda9718a243549f621a21adc",
+    "url": "/wealth-tax/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wealth-tax/precache-manifest.cac586758a79010815c7157635fb6bb1.js"
+  "/wealth-tax/precache-manifest.982c42a4a8ebf76f3f162c6fe3ad7c65.js"
 );
 
 workbox.clientsClaim();
